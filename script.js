d0fce88222e5439729558831c5b63e7bdcfc0697
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-09-19 20:58:53 -0400

    added functionality to the program

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById("genderInput").value;
+document.getElementById("ageInput").value;
+document.getElementById("heightInput").value;
+document.getElementById("weightInput").value;
+document.getElementById("outputDetails").innerHTML;
+
+function calculate() {
+    let output = document.getElementById("outputDetails").innerHTML;
+    var gender = document.getElementById("genderInput").value;
+    var age = document.getElementById("ageInput").value;
+    var height = document.getElementById("heightInput").value;
+    var weight = document.getElementById("weightInput").value;
+
+    output = (10 * weight) + (6.25 * height) - (5 * age);
+
+    if (gender == "male") {
+        output = output + 5;
+    } else if (gender == "female") {
+        output = output - 161;
+    } else {
+        gender == "Prefer not to say";
+        return;
+    }
+
+    document.getElementById("outputDetails").innerHTML = `Your estimated daily calorie needs: ${output} calories`;
+    return;
+}
+
+function reset() {
+    document.getElementById("genderInput").value = "";
+    document.getElementById("ageInput").value = "";
+    document.getElementById("heightInput").value = "";
+    document.getElementById("weightInput").value = "";
+    document.getElementById("outputDetails").innerHTML = "";
+}
